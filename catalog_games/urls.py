20ff61c952fb1a107790d0840d60a812e45b45f1
Carlos Daniel Sousa <danyelpinheiro154@hotmail.com>
2022-03-28 10:31:41 -0300

    app de accounts e view de cadastro criadas e arquivo base.html editado

--- catalog_games/urls.py
+++ catalog_games/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 
 
 urlpatterns = [
+    path('', include('core.urls')),
+    path('accounts/', include('accounts.urls')),
     path('admin/', admin.site.urls),
-    path('', include('core.urls'))
 ]
