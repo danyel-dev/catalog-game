037a02a3b9b6c403ce53f0c7ec222be210fe2d47
Carlos Daniel Sousa <danyelpinheiro154@hotmail.com>
2022-03-30 17:01:50 -0300

    adicionando o campo de email ao UserCreationForm

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
-# from django.http import HttpResponse
-from django.contrib.auth.models import User
+from django.shortcuts import redirect, render
+from django.http import HttpResponse
+
+from .forms import UserCreationForm
 
 
 def register(request):  
-    return render(request, 'accounts/register.html')
+    form = UserCreationForm(request.POST or None)
+
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    return render(request, 'accounts/register.html', {'form': form})
