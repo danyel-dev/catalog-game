4f7a26ad1f0d9ab7d5e846b091e8517fbae66501
Carlos Daniel Sousa <danyelpinheiro154@hotmail.com>
2022-03-31 11:00:42 -0300

    tela de cadastro pronta

--- catalog_games/settings.py
+++ catalog_games/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'core',
     'accounts',
     'compressor',
+    'crispy_forms',
 ]
 
 # Django compressor
@@ -135,6 +136,9 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/')]
+
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
---
+++ core/static/core/js/register.js
@@ -0,0 +1,4 @@
+document.getElementById('id_username').setAttribute('placeholder', 'Nome de usuário')
+document.getElementById('id_email').setAttribute('placeholder', 'Endereço de E-mail')
+document.getElementById('id_password1').setAttribute('placeholder', 'Crie uma senha')
+document.getElementById('id_password2').setAttribute('placeholder', 'Digite a senha novamente')
