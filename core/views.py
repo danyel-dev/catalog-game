8e705ba523e155fca3a4494df6d7746f3fd5a75a
Carlos Daniel Sousa <danyelpinheiro154@hotmail.com>
2022-04-11 23:45:25 -0300

    view de contato criada

--- core/views.py
+++ core/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from email import message
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
+from django.contrib import messages
 from .forms import MessageForm
 
 
@@ -12,5 +14,15 @@ def about(request):
 
 
 def contact(request):
-    form = MessageForm()
+    form = MessageForm(request.POST or None)
+
+    if request.method == 'POST':
+        if form.is_valid():
+            form = form.save(commit=False)
+            form.user = request.user
+            form.save()
+            messages.success(request, 'Sua mensagem foi enviada com sucesso!')
+        
+        return redirect('contact')
+
     return render(request, 'core/contact.html', {'form': form})
