96d21aaba0a160f469734a38dce40e3f7b0d6c30
Carlos Daniel Sousa <danyelpinheiro154@hotmail.com>
2022-04-13 10:45:04 -0300

    a

--- core/models.py
+++ core/models.py
@@ -13,9 +13,9 @@ class Message(models.Model):
 class Game(models.Model):
     title = models.CharField(max_length=100, verbose_name='Título do Game')
     about = models.TextField(verbose_name='Sobre o jogo')
-    distributor = models.CharField(max_length=100, verbose_name='Distribuidora')
     developer = models.CharField(max_length=100, verbose_name='Desenvolvedora')
-    date_lauch = models.DateTimeField(verbose_name='Data de lançamento')
+    distributor = models.CharField(max_length=100, verbose_name='Distribuidora')
+    date_lauch = models.DateField(verbose_name='Data de lançamento')
     image = models.ImageField(upload_to='game_img/%Y/%m/%d', verbose_name='Imagem')
     tags = TaggableManager(verbose_name='Gêneros')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Data de envio')
