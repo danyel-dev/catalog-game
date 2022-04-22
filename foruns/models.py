18cff21c9396b6dddfda8fb00d01a34626223153
Carlos Daniel Sousa <danyelpinheiro154@hotmail.com>
2022-04-22 07:26:45 -0300

    campo de tags removido do model Forum

--- foruns/models.py
+++ foruns/models.py
@@ -7,7 +7,6 @@ class Forum(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Usuário')
     title = models.CharField('Título', max_length=100)  
     description = models.TextField('Descrição')
-    tags = TaggableManager()
     image = models.ImageField('Imagem', upload_to='forum_img/%Y/%m/%d')
     created_at = models.DateTimeField('Data de criação', auto_now_add=True)
     updated_at = models.DateTimeField('Data de atualização', auto_now=True)
